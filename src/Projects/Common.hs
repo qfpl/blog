ab5cab0dfed9a578076f0119087d4642e60997b7
Dave Laing <dave.laing.80@gmail.com>
2017-07-11 12:52:04 +1000

    Fixes up some license and copyright info.

--- src/Projects/Common.hs
+++ src/Projects/Common.hs
@@ -1,10 +1,3 @@
-{-|
-Copyright   : (c) Dave Laing, 2017
-License     : BSD3
-Maintainer  : dave.laing.80@gmail.com
-Stability   : experimental
-Portability : non-portable
--}
 {-# LANGUAGE OverloadedStrings #-}
 module Projects.Common (
     lookupProject
