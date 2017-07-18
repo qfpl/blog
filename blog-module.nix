eeb537254ccd8e92c87b0a8c1d210b835ddd788d
Dave Laing <dave.laing.80@gmail.com>
2017-07-18 11:04:20 +1000

    Updates the blog module

--- blog-module.nix
+++ blog-module.nix
@@ -3,60 +3,62 @@
 with lib;
 
 let 
-  cfg = config.services.blog;
+  cfg = config.services.blog-updater;
 in
 {
   # interface
   options = {
 
-    services.blog = rec {
+    services.blog-updater = rec {
 
       enable = mkOption {
         type = types.bool;
         default = false;
         description = ''
-          Whether to run the blog service;
+          Whether to run the blog updating service
         '';
       };
 
-      package = mkOption {
-        type = types.path;
-        description = ''
-          The blog package;
-        '';
-      };
-
-      prefix = mkOption {
+      virtualHost = mkOption {
         type = types.str;
-        default = "/blog";
+        default = "blog.qfpl.io";
         description = ''
-          The url to the blog from the root of the webserver.
+          The virtual host to serve the blog content from
         '';
       };
 
-      datadir = mkOption {
+      hydraJob = mkOption {
         type = types.str;
-        default = "${cfg.package}/blog";
+        default = "http://hydra.qfpl.io/job/team-blog/blog/blog";
         description = ''
-          The path to the assets for the blog.
+          The url to the hydra job for the blog content
         '';
       };
+
+      blogProfile = mkOption {
+        type = types.str;
+        default = "blog";
+        description = ''
+          The profile to run hail from
+        '';
+      };
+
     };
   };
 
   # implementation
   config = mkIf cfg.enable {
 
-    services.blog.package = mkDefault ((import ./default.nix));  
-    services.nginx = {
-      enable = true;
-      recommendedOptimisation = true;
-      recommendedTlsSettings = true;
-      recommendedGzipSettings = true;
-      recommendedProxySettings = true;
-      virtualHosts."localhost" = {
-        locations.${cfg.prefix}.alias = "${cfg.datadir}";
-      }; 
+    services.nginx.virtualHosts."${cfg.virtualHost}".locations."/".root = "/nix/var/nix/profiles/${cfg.blogProfile}/blog/";
+
+    systemd.services.blog-updater = {
+      description = "Team blog updater";
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      stopIfChanged = false;
+      serviceConfig = {
+        ExecStart = "${pkgs.haskellPackages.hail}/bin/hail --profile ${cfg.blogProfile} --job-uri ${cfg.hydraJob}";
+      };
     };
 
   }; 
